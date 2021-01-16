b5cf3eea8b0480af06e047226bdfe51a67b880e7
PhanQuocKhanh125 <quockhanhd3k12@gmail.com>
2021-01-16 11:27:07 +0700

    first commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Nhap vao a: ");
+            float a = float.Parse(Console.ReadLine());
+
+            if (a == 0)
+            {
+                Console.WriteLine("a phai khac 0");
+            }
+            else
+            {
+                Console.WriteLine("Nhap vao b: ");
+                float b = float.Parse(Console.ReadLine());
+
+                float x = -b / a;
+                Console.WriteLine("{0}x + {1} = 0 \n=> x = {2}", a, b, x);
+            }
+            Console.ReadKey();
+        }
+    }
+}
