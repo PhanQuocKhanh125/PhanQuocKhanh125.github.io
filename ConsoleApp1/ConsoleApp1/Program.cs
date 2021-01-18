f355c7ae6bab6e8c2c922673bd371b25ca53ad22
PhanQuocKhanh125 <quockhanhd3k12@gmail.com>
2021-01-18 21:06:39 +0700

    LTQL

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello World!");
+            Console.Write("Ten nguoi dung: ");
+            Console.WriteLine("Chao mung " + Console.ReadLine() + " den voi mon hoc LTQL");
+        }
+    }
+}
